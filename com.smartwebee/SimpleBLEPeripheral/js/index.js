95634f39276febd0950641da07c70fbf56db6511
ChengCX <ccxysfh1993@gmail.com>
2014-07-30 12:16:15 +0800

    SimpleBLEPeripheral bluetooth module app.

---
+++ com.smartwebee/SimpleBLEPeripheral/js/index.js
@@ -0,0 +1,127 @@
+/*
+	Copyright 2013-2014, JUMA Technology
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+var app = {
+
+    // Application Constructor
+    initialize: function() {
+        app.bindCordovaEvents();
+    },
+    
+    bindCordovaEvents: function() {
+		document.addEventListener('deviceready',app.onDeviceReady,false);
+        document.addEventListener('bcready', app.onBCReady, false);
+    },
+    
+	onDeviceReady : function(){
+		var BC = window.BC = cordova.require("org.bcsphere.bcjs");
+	},
+	
+	onBCReady : function(){
+
+	},
+	
+	write : function(){
+		var device = new BC.Device({deviceAddress:"78:C5:E5:99:26:37",type:"BLE"});
+		device.connect(function(){
+			device.discoverServices(function(){
+				var service = device.getServiceByUUID("fff0")[0];
+				service.discoverCharacteristics(function(){
+					var character = service.getCharacteristicByUUID("fff1")[0];
+					var text=document.getElementById("youWrite").value;
+					character.write("ASCII",text,function(data){
+						alert(JSON.stringify(data));
+					},function(){
+						alert("write error!");
+					});
+				},function(){
+					alert("discoverCharacteristics error!");
+				});
+			},function(){
+				alert("discoverServices error!");
+			});
+		},function(){
+			alert("connnect error!");
+		});
+	},
+	read : function(){
+		var device = new BC.Device({deviceAddress:"78:C5:E5:99:26:37",type:"BLE"});
+		device.connect(function(){
+			device.discoverServices(function(){
+				var service = device.getServiceByUUID("fff0")[0];
+				service.discoverCharacteristics(function(){
+					var character = service.getCharacteristicByUUID("fff1")[0];
+					character.read(function(data){
+						alert(JSON.stringify(data));
+						document.getElementById("charactisticArea").innerHTML=JSON.stringify(data.value.getASCIIString());
+						alert(JSON.stringify(data));
+					},function(){
+						alert("read error!");
+					});
+				},function(){
+					alert("discoverCharacteristics error!");
+				});
+			},function(){
+				alert("discoverServices error!");
+			});
+		},function(){
+			alert("connnect error!");
+		});
+	},
+	subscribe : function(){
+		var device = new BC.Device({deviceAddress:"78:C5:E5:99:26:37",type:"BLE"});
+		device.connect(function(){
+			device.discoverServices(function(){
+				var service = device.getServiceByUUID("fff0")[0];
+				service.discoverCharacteristics(function(){
+					var character = service.getCharacteristicByUUID("fff7")[0];					
+					character.subscribe(function(data){
+						alert("subscribe success");
+						document.getElementById("subscribeArea").innerHTML=JSON.stringify(data);
+					});
+			},function(){
+					alert("discoverCharacteristics error!");
+				});
+			},function(){
+				alert("discoverServices error!");
+			});
+		},function(){
+			alert("connnect error!");
+		});
+	},
+	unsubscribe : function(){
+		var device = new BC.Device({deviceAddress:"78:C5:E5:99:26:37",type:"BLE"});
+		device.connect(function(){
+			device.discoverServices(function(){
+				var service = device.getServiceByUUID("fff0")[0];
+				service.discoverCharacteristics(function(){
+					var character = service.getCharacteristicByUUID("fff7")[0];					
+					character.unsubscribe(function(data){
+						alert("unsubscribe success");					
+					},function(){
+						alert("unsubscribe error");
+					});
+			},function(){
+					alert("discoverCharacteristics error!");
+				});
+			},function(){
+				alert("discoverServices error!");
+			});
+		},function(){
+			alert("connnect error!");
+		});
+	},
+};
